49e5d55410ab374b2ce13248e8303ec4071df492
Dan Hoying <danhoying@gmail.com>
2016-07-27 07:56:05 -0700

    Add append and prepend methods

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,7 @@
 class Node
   attr_accessor :value, :next_node
   def initialize(value)
-    @value = nil
+    @value = value
     @next_node = nil
   end
 end
@@ -11,5 +11,32 @@ class LinkedList
     @head = nil
     @tail = nil
   end
+
+  def append(data)
+     if @head.nil?
+       @head = data
+       @tail = data
+     else
+       @tail.next_node = data
+       @tail = data
+     end
+  end
+
+  def prepend(data)
+    if @head.nil?
+      @head = data
+      @tail = data
+    else
+      data.next_node = @head
+      @head = data
+    end
+  end
 end
+
+list = LinkedList.new
+list.append(Node.new(5))
+list.append(Node.new(7))
+list.prepend(Node.new(14))
+list.append(Node.new(34))
+p list 
 
